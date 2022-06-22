c2abf3b70fd38e7f80a1a1e1db0c0eecab83f86b
Tony5300 <tony_tolen@ziggo.nl>
2022-06-22 16:36:53 +0200

    added some minor changes to the holes. Changed the size of the holes and the amount of holes. First there were four holes next to eachother, now there are three holes next to eachother. Also added a black background color inside of the hole.

--- src/mole.js
+++ src/mole.js
@@ -6,16 +6,19 @@ class Mole {
 
   //1. draw the hole and the moles
   drawMole() {
+    push();
+    fill("black");
     this.drawHoles();
+    pop();
   }
   // 2.create a hole where the digletts are hiding
   drawHoles() {
     translate(this.width / 1.5, this.height / 4); // with the width value of 300
 
-    for (let x = 0; x <= this.width; x = x += 100) {
-      circle(x, 50, 50, 50);
-      circle(x, 150, 50, 50);
-      circle(x, 250, 50, 50);
-    }
+    for (let x = 0; x <= this.width; x = x += 150) {
+      circle(x, 50, 65, 50); // top holes
+      circle(x, 150, 65, 50); // middle holes
+      circle(x, 250, 65, 50); // bottom holes
+    } // create holes
   }
 }
